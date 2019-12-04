a97eb0a41e29c00bad1148147739dc81b9010e8c
nsalvador <nigelsalvador@gmail.com>
2019-12-03 21:10:27 -0500

    '/users' & '/users/login' routes test cases

--- tests/show.test.js
+++ tests/show.test.js
@@ -1,42 +1,43 @@
 const request = require('supertest');
 const app = require('../src/app');
 
-describe("'/shows/search' route", () => {
-	test('Should search for a show', async () => {
-		const response = await request(app)
-			.post('/shows/search?page=1')
-			.send({
-				show: 'blue bloods'
-			})
-			.expect(200);
-		expect(response.body.series.length).not.toBe(0);
-		expect(response.body.page).toBe(1);
-	});
+test('Should search for a show', async () => {
+	const response = await request(app)
+		.post('/shows/search?page=1')
+		.send({
+			show: 'blue bloods'
+		})
+		.expect(200);
+	expect(response.body.series.length).not.toBe(0);
+	expect(response.body.page).toBe(1);
+});
 
-	test('Should not be able to find show', async () => {
-		await request(app)
-			.post('/shows/search?page=1')
-			.send({
-				show: 'this_show_does_not_exist'
-			})
-			.expect(404);
-	});
+test('Should not be able to find show', async () => {
+	await request(app)
+		.post('/shows/search?page=1')
+		.send({
+			show: 'this_show_does_not_exist'
+		})
+		.expect(404);
+});
 
-	test('Should return the nth page of a searched show', async () => {
-		let response = null;
-		const showObj = { show: 'suits' };
-		const pages = null,
-			page = null;
+test('Should return the nth page of a searched show', async () => {
+	const showObj = { show: 'suits' };
+	let response = null;
+	let pages = null,
+		page = null;
 
-		response = await request(app)
-			.post('/shows/search?page=1')
-			.send(showObj)
-			.expect(200);
-		pages = response.body.pages;
-		page = Math.floor(Math.random() * pages + 1);
-		response = await request(app)
-			.post(`/shows/search?page=${page}`)
-			.send(showObj);
-		expect(response.body.page).toBe(page);
-	});
+	response = await request(app)
+		.post('/shows/search?page=1')
+		.send(showObj)
+		.expect(200);
+
+	pages = response.body.pages;
+	page = Math.floor(Math.random() * pages + 1);
+
+	response = await request(app)
+		.post(`/shows/search?page=${page}`)
+		.send(showObj)
+		.expect(200);
+	expect(response.body.page).toBe(page);
 });
